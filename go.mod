8d3787ceab83b4550a2b8ac8039446bbfb65d65f
k8sland <fernand@imhotep.io>
2019-02-21 16:43:25 -0700

    page support+text view highlight+lnf

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/derailed/tview
+
+replace github.com/rivo/tview => /Users/fernand/go_wk/derailed/src/github.com/derailed/tview
+
+require (
+	github.com/gdamore/tcell v1.1.1
+	github.com/lucasb-eyer/go-colorful v0.0.0-20181028223441-12d3b2882a08
+	github.com/mattn/go-runewidth v0.0.4
+	github.com/rivo/tview v0.0.0-20190213202703-b373355e9db4
+)
--- styles.go
+++ styles.go
@@ -13,6 +13,7 @@ var Styles = struct {
 	MoreContrastBackgroundColor tcell.Color // Background color for even more contrasting elements.
 	BorderColor                 tcell.Color // Box borders.
 	TitleColor                  tcell.Color // Box titles.
+	FocusColor                  tcell.Color // Box focus color.
 	GraphicsColor               tcell.Color // Graphics.
 	PrimaryTextColor            tcell.Color // Primary text.
 	SecondaryTextColor          tcell.Color // Secondary text (e.g. labels).
@@ -25,6 +26,7 @@ var Styles = struct {
 	MoreContrastBackgroundColor: tcell.ColorGreen,
 	BorderColor:                 tcell.ColorWhite,
 	TitleColor:                  tcell.ColorWhite,
+	FocusColor:                  tcell.ColorGreen,
 	GraphicsColor:               tcell.ColorWhite,
 	PrimaryTextColor:            tcell.ColorWhite,
 	SecondaryTextColor:          tcell.ColorYellow,
